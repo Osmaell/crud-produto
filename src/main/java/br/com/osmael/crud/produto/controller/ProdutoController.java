bd18b3f07f9ec4a4dd2282517af72e728164ee4d
Osmael de Sousa Braga <osmaelsousa.ti@gmail.com>
2017-02-17 01:46:12 -0300

    Tela de cadastro de produto
    Criação da tela de cadastro de produto e vinculação de alguns recursos
    de estilização.

---
+++ src/main/java/br/com/osmael/crud/produto/controller/ProdutoController.java
@@ -0,0 +1,15 @@
+package br.com.osmael.crud.produto.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/produtos")
+public class ProdutoController {
+	
+	@RequestMapping("/novo")
+	public String novo() {
+		return "CadastroProduto";
+	}
+	
+}
