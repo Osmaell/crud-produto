ac39c1003ce8e90f8943d22249fb1dddd72422f9
Osmael de Sousa Braga <osmaelsousa.ti@gmail.com>
2017-02-17 00:15:05 -0300

    Commit Inicial

---
+++ src/main/java/br/com/osmael/crud/produto/CrudProdutoApplication.java
@@ -0,0 +1,12 @@
+package br.com.osmael.crud.produto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudProdutoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudProdutoApplication.class, args);
+	}
+}
