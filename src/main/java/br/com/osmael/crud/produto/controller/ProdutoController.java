fb35b0d7c999bd67e51e91dd2028f305e5cf254f
Osmael de Sousa Braga <osmaelsousa.ti@gmail.com>
2017-02-17 02:32:34 -0300

    Tela de listagem de produto
    Criação da tela de listagem e ajuste de menus.

--- src/main/java/br/com/osmael/crud/produto/controller/ProdutoController.java
+++ src/main/java/br/com/osmael/crud/produto/controller/ProdutoController.java
@@ -1,8 +1,8 @@
 package br.com.osmael.crud.produto.controller;
-
+	
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-
+	
 @Controller
 @RequestMapping("/produtos")
 public class ProdutoController {
@@ -10,6 +10,11 @@ public class ProdutoController {
 	@RequestMapping("/novo")
 	public String novo() {
 		return "CadastroProduto";
+	}
+	
+	@RequestMapping
+	public String pesquisar() {
+		return "PesquisaProduto";
 	}
 	
 }
